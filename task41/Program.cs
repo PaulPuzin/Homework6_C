e7714a0de39cdb3414ac589e5466037d32904fe3
Paul Puzin <>
2022-09-12 00:28:42 +0300

    added info about task41

---
+++ task41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите размерность массива (число)");
+int ElementsCount = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [ElementsCount];
+for (int i = 0; i < array.Length; i++)
+{
+Console.WriteLine ("Введите число");   
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int sum = 0;
+for (int i=0;i<array.Length;i++)
+{
+    if (array[i]>0)
+    {
+        sum=sum+1;
+    }
+}   
+Console.WriteLine($"Количество чисел введенных пользователем больше нуля равно {sum}");
